166c3b7a4d376af8687e7afabc9c12a00c41fc1c
Laurent Berge <laurent.berge@u-bordeaux.fr>
2026-06-26 13:10:02 +0000

    version 0.14.2

--- inst/doc/exporting_tables.R
+++ inst/doc/exporting_tables.R
@@ -8,6 +8,9 @@ library(fixest)
 setFixest_notes(FALSE)
 setFixest_etable(digits = 3)
 
+# Can we compile LaTeX to PNG? Not available during R CMD check.
+can_markdown = !nzchar(Sys.getenv("_R_CHECK_PACKAGE_NAME_", ""))
+
 ## ----eval = TRUE, results = "hide"------------------------------------------------------
 library(fixest)
 data(airquality)
@@ -44,18 +47,17 @@ my_style = style.df(depvar.title = "", fixef.title = "",
                     fixef.suffix = " fixed effect", yesNo = "yes")
 setFixest_etable(style.df = my_style, postprocess.df = pandoc.table.return)
 
-## ----eval = is_pand, eval = is_pander---------------------------------------------------
+## ----eval = is_pander-------------------------------------------------------------------
 etable(est[rhs = 2], style = "rmarkdown", caption = "New default values")
 
 ## ---------------------------------------------------------------------------------------
 est_slopes = feols(Ozone ~ Solar.R + Wind | Day + Month[Temp], airquality)
 
-## ----results = 'hide'-------------------------------------------------------------------
+## ---------------------------------------------------------------------------------------
 etable(est, est_slopes, tex = TRUE)
 
-## ----include = FALSE--------------------------------------------------------------------
-# etable(est, est_slopes, file = "../_VIGNETTES/vignette_etable.tex", replace = TRUE)
-# etable(est, est_slopes, file = "../_VIGNETTES/vignette_etable.tex", style.tex = style.tex("aer"), fitstat = ~ r2 + n, signif.code = NA)
+## ----eval = FALSE-----------------------------------------------------------------------
+# etable(est, est_slopes, file = "path/to/mytable.tex")
 
 ## ----results = 'hide'-------------------------------------------------------------------
 etable(est, est_slopes, style.tex = style.tex("aer"), 
@@ -80,17 +82,86 @@ set_rules = function(x, heavy, light){
   x
 }
 
-## ----results = 'hide'-------------------------------------------------------------------
+## ----include = FALSE--------------------------------------------------------------------
+if (can_markdown) setFixest_etable(markdown = TRUE, page.width = "a4")
+
+## ----results = 'asis'-------------------------------------------------------------------
 etable(est, est_slopes, postprocess.tex = set_rules, heavy = "0.14em", tex = TRUE)
 
 ## ---------------------------------------------------------------------------------------
 setFixest_etable(style.tex = style.tex("aer", signif.code = NA), postprocess.tex = set_rules, 
                  fitstat = ~ r2 + n)
 
-## ---------------------------------------------------------------------------------------
+## ----results = 'asis'-------------------------------------------------------------------
 etable(est, heavy = "0.14em", tex = TRUE)
 
+## ----results = 'asis'-------------------------------------------------------------------
+nm = names(iris)
+est_iris = feols(.[nm[1]] ~ .[nm[2:4]], iris, fsplit = ~Species)
+
+etable(est_iris, arraystretch = 1.5, tex = TRUE,
+       highlight = .("Sepal@1", 
+                     "cyan4, square" = "Petal.L@3-4",
+                     "thick5, sep8, darkgreen!90, se" = "Petal.W"))
+
+## ----results = 'asis'-------------------------------------------------------------------
+etable(est_iris, arraystretch = 1.5, tex = TRUE,
+       highlight = .(rowcol = "Sepal@1", 
+                     "rowcol, cyan4!70" = "Petal.L@3-4",
+                     "rowcol, darkgreen!40, se" = "Petal.W"))
+
+## ----results = 'asis'-------------------------------------------------------------------
+etable(est_iris, tex = TRUE,
+       coef.style = .(":coef:$\\bigstar$" = "Sepal@1", 
+                      "**:coef:**" = "Petal.L@3-4",
+                      "\\color{BrickRed} :coef_se:" = "Petal.W"))
+
+## ----results = 'asis'-------------------------------------------------------------------
+# First define notes in the dictionary
+setFixest_dict(c(note1 = paste0("*Notes*: This is a note that illustrates how to access notes ",
+                                "from the dictionary."),
+                 source = "*Sources*: Somewhere from the net."))
+
+etable(est, 
+       style.tex = style.tex("aer", tpt = TRUE, notes.tpt.intro = "\\footnotesize"),
+       notes = c("note1", "source"), tex = TRUE)
+
+## ----results = 'asis'-------------------------------------------------------------------
+# Setting up tpt globally
+my_style = style.tex(tpt = TRUE, 
+                     notes.tpt.intro = "\\footnotesize")
+setFixest_etable(style.tex = my_style)
+
+# Below is identical to: 
+#  etable(est, 
+#         style.tex = style.tex(notes.tpt.intro = "\\Large"),
+#         notes = "These notes are large.")
+etable(est, notes = c("@\\Large", "These notes are large."), tex = TRUE)
+
+## ----results = 'asis'-------------------------------------------------------------------
+est_many = feols(Ozone ~ mvsw(Solar.R, Wind, Temp), airquality)
+etable(est_many, adjustbox = 1.1, tex = TRUE)
+
 ## ---------------------------------------------------------------------------------------
+# Reset defaults from previous sections
+setFixest_etable(reset = TRUE)
+
+## ----include = FALSE--------------------------------------------------------------------
+if (can_markdown) setFixest_etable(markdown = TRUE, page.width = "a4")
+
+## ----results = 'asis'-------------------------------------------------------------------
+etable(est, headers = .("\n\n Short header" = 2, "*Very* \n **long** \n ***header***" = 2),
+       tex = TRUE)
+
+## ----include = FALSE--------------------------------------------------------------------
+if (can_markdown) setFixest_etable(markdown = FALSE)
+
+## ---------------------------------------------------------------------------------------
+# Reset defaults from previous sections
+setFixest_etable(reset = TRUE)
+setFixest_dict(c(Ozone = "Ozone (ppb)", Solar.R = "Solar Radiation (Langleys)",
+                 Wind = "Wind Speed (mph)", Temp = "Temperature"))
+
 fitstat_register(type = "p_s", alias = "pvalue (standard)",
                  fun = function(x) pvalue(x, vcov = "iid")["Solar.R"])
 
@@ -103,8 +174,6 @@ fitstat_register(type = "p_day", alias = "pvalue (Day)",
 fitstat_register(type = "p_month", alias = "pvalue (Month)",
                  fun = function(x) pvalue(x, vcov = ~Month)["Solar.R"])
 
-# We first reset the default values set in the previous sections
-setFixest_etable(reset = TRUE)
 # Now we display the results with the new fit statistics
 etable(est, fitstat = ~ . + p_s + p_h + p_day + p_month)
 
--- inst/doc/standard_errors.R
+++ inst/doc/standard_errors.R
@@ -1,17 +1,40 @@
 ## ----setup, include = FALSE-------------------------------------------------------------
-knitr::opts_chunk$set(echo = TRUE, 
-                      eval = TRUE,
-                      comment = "#>")
+knitr::opts_chunk$set(echo = TRUE, eval = TRUE, comment = "#>")
 Sys.setenv(lang = "en")
-
 library(fixest)
 setFixest_nthreads(1)
 
 ## ---------------------------------------------------------------------------------------
 library(fixest)
+est = feols(mpg ~ wt + hp, mtcars)
+
+## Simple function that computes a Bayesian bootstrap
+vcov_bboot = function(x, n_sim = 100) {
+  core_env = update(x, only.coef = TRUE, only.env = TRUE)
+  n_obs = x$nobs
+
+  res_all = vector("list", n_sim)
+  for (i in 1:n_sim) {
+    # est_env updates estimate using new weights
+    res_all[[i]] = est_env(env = core_env, weights = rexp(n_obs, rate = 1))
+  }
+
+  res = do.call(rbind, res_all)
+
+  # Could return just `var(res)` but the named list will display a nice name in `etable`
+  return(list("Bayesian Bootstrap" = var(res)))
+}
+
+set.seed(1)
+est_bboot_se = summary(est, vcov = vcov_bboot(est))
+etable(est, est_bboot_se)
+
+## ---------------------------------------------------------------------------------------
 data(trade)
-# OLS estimation
-gravity = feols(log(Euros) ~ log(dist_km) | Destination + Origin + Product + Year, trade)
+gravity = feols(
+  log(Euros) ~ log(dist_km) | Destination + Origin + Product + Year,
+  trade
+)
 # Two-way clustered SEs
 summary(gravity, vcov = "twoway")
 # Two-way clustered SEs, without small sample correction
@@ -20,12 +43,12 @@ summary(gravity, vcov = "twoway", ssc = ssc(K.adj = FALSE, G.adj = FALSE))
 ## ----eval = TRUE, include = FALSE-------------------------------------------------------
 is_plm = requireNamespace("plm", quietly = TRUE)
 
-if(!is_plm){
+if (!is_plm) {
   knitr::opts_chunk$set(eval = FALSE)
   cat("Evaluation of the next chunks requires 'plm' which is not installed.")
 } else {
   knitr::opts_chunk$set(eval = TRUE)
-  
+
   library(plm)
   library(sandwich)
 }
@@ -43,25 +66,33 @@ library(plm)
 data(Grunfeld)
 
 # Estimations
-res_lm    = lm(inv ~ capital, Grunfeld)
+res_lm = lm(inv ~ capital, Grunfeld)
 res_feols = feols(inv ~ capital, Grunfeld)
 
 # Same standard-errors
 rbind(se(res_lm), se(res_feols))
 
 # Heteroskedasticity-robust covariance
-se_lm_hc    = sqrt(diag(vcovHC(res_lm, type = "HC1")))
+se_lm_hc = sqrt(diag(vcovHC(res_lm, type = "HC1")))
 se_feols_hc = se(res_feols, vcov = "hetero")
 rbind(se_lm_hc, se_feols_hc)
 
 ## ---------------------------------------------------------------------------------------
 
 # Estimations
-est_lm    = lm(inv ~ capital + as.factor(firm) + as.factor(year), Grunfeld)
-est_plm   = plm(inv ~ capital + as.factor(year), Grunfeld, 
-                index = c("firm", "year"), model = "within")
+est_lm = lm(inv ~ capital + as.factor(firm) + as.factor(year), Grunfeld)
+est_plm = plm(
+  inv ~ capital + as.factor(year),
+  Grunfeld,
+  index = c("firm", "year"),
+  model = "within"
+)
 # we use panel.id so that panel VCOVs can be applied directly
-est_feols = feols(inv ~ capital | firm + year, Grunfeld, panel.id = ~firm + year)
+est_feols = feols(
+  inv ~ capital | firm + year,
+  Grunfeld,
+  panel.id = ~ firm + year
+)
 
 #
 # "iid" standard-errors
@@ -71,14 +102,18 @@ est_feols = feols(inv ~ capital | firm + year, Grunfeld, panel.id = ~firm + year
 rbind(se(est_lm)["capital"], se(est_plm)["capital"], se(est_feols))
 
 # p-values:
-rbind(pvalue(est_lm)["capital"], pvalue(est_plm)["capital"], pvalue(est_feols, vcov = "iid"))
+rbind(
+  pvalue(est_lm)["capital"],
+  pvalue(est_plm)["capital"],
+  pvalue(est_feols, vcov = "iid")
+)
 
 
 ## ---------------------------------------------------------------------------------------
 # Clustered by firm
-se_lm_firm    = se(vcovCL(est_lm, cluster = ~firm, type = "HC1"))["capital"]
-se_plm_firm   = se(vcovHC(est_plm, cluster = "group"))["capital"]
-se_stata_firm = 0.06328129    # vce(cluster firm)
+se_lm_firm = se(vcovCL(est_lm, cluster = ~firm, type = "HC1"))["capital"]
+se_plm_firm = se(vcovHC(est_plm, cluster = "group"))["capital"]
+se_stata_firm = 0.06328129 # vce(cluster firm)
 se_feols_firm = se(est_feols, vcov = ~firm)
 
 rbind(se_lm_firm, se_plm_firm, se_stata_firm, se_feols_firm)
@@ -89,7 +124,11 @@ se_feols_firm_lm = se(est_feols, vcov = ~firm, ssc = ssc(K.fixef = "full"))
 rbind(se_lm_firm, se_feols_firm_lm)
 
 # How to get the plm version
-se_feols_firm_plm = se(est_feols, vcov = ~firm, ssc = ssc(K.fixef = "none", G.adj = FALSE))
+se_feols_firm_plm = se(
+  est_feols,
+  vcov = ~firm,
+  ssc = ssc(K.fixef = "none", G.adj = FALSE)
+)
 rbind(se_plm_firm, se_feols_firm_plm)
 
 ## ---------------------------------------------------------------------------------------
@@ -97,43 +136,45 @@ rbind(se_plm_firm, se_feols_firm_plm)
 # Newey-west
 #
 
-se_plm_NW   = se(vcovNW(est_plm))["capital"]
+se_plm_NW = se(vcovNW(est_plm))["capital"]
 se_feols_NW = se(est_feols, vcov = "NW")
 
 rbind(se_plm_NW, se_feols_NW)
 
 # we can replicate plm's by changing the type of SSC:
-rbind(se_plm_NW, 
-      se(est_feols, vcov = NW ~ ssc(K.adj = FALSE, G.adj = FALSE)))
+rbind(se_plm_NW, se(est_feols, vcov = NW ~ ssc(K.adj = FALSE, G.adj = FALSE)))
 
 #
 # Driscoll-Kraay
 #
 
-se_plm_DK   = se(vcovSCC(est_plm))["capital"]
+se_plm_DK = se(vcovSCC(est_plm))["capital"]
 se_feols_DK = se(est_feols, vcov = "DK")
 
 rbind(se_plm_DK, se_feols_DK)
 
 # Replicating plm's
-rbind(se_plm_DK, 
-      se(est_feols, vcov = DK ~ ssc(K.adj = FALSE, G.adj = FALSE)))
+rbind(se_plm_DK, se(est_feols, vcov = DK ~ ssc(K.adj = FALSE, G.adj = FALSE)))
 
 
 ## ----eval = TRUE, include = FALSE-------------------------------------------------------
 is_lfe = requireNamespace("lfe", quietly = TRUE)
 is_lfe_plm = is_lfe && is_plm
-if(is_lfe){
-    # avoids ugly startup messages popping + does not require the use of the not very elegant suppressPackageStartupMessages
-    library(lfe)
+if (is_lfe) {
+  # avoids ugly startup messages popping + does not require the use of the not very elegant suppressPackageStartupMessages
+  library(lfe)
 }
 
 ## ----eval = !is_lfe_plm, include = !is_lfe_plm, echo = FALSE----------------------------
-# if(!is_lfe){
-#   cat("The evaluation of the next chunks of code requires the package 'lfe' which is not installed")
+# if (!is_lfe) {
+#   cat(
+#     "The evaluation of the next chunks of code requires the package 'lfe' which is not installed"
+#   )
 # } else {
-#   cat("The evaluation of the next chunks of code requires the package 'plm' (for the data set) which is not installed.",
-#     "\nThe code output is not reported.")
+#   cat(
+#     "The evaluation of the next chunks of code requires the package 'plm' (for the data set) which is not installed.",
+#     "\nThe code output is not reported."
+#   )
 # }
 
 ## ----eval = is_lfe_plm, warning = FALSE-------------------------------------------------
@@ -148,16 +189,20 @@ rbind(se_lfe_firm, se_feols_firm)
 
 # You have to provide a custom VCOV to replicate lfe's VCOV
 my_vcov = vcov(est_feols, vcov = ~firm, ssc = ssc(K.adj = FALSE))
-se(est_feols, vcov = my_vcov * 199/198) # Note that there are 200 observations
+se(est_feols, vcov = my_vcov * 199 / 198) # Note that there are 200 observations
 
 # Differently from feols, the SEs in lfe are different if year is not a FE:
 # => now SEs are identical.
-rbind(se(felm(inv ~ capital + factor(year) | firm | 0 | firm, Grunfeld))["capital"],
-      se(feols(inv ~ capital + factor(year) | firm, Grunfeld), vcov = ~firm)["capital"])
+rbind(
+  se(felm(inv ~ capital + factor(year) | firm | 0 | firm, Grunfeld))["capital"],
+  se(feols(inv ~ capital + factor(year) | firm, Grunfeld), vcov = ~firm)[
+    "capital"
+  ]
+)
 
 # Now with two-way clustered standard-errors
-est_lfe_2way  = felm(inv ~ capital | firm + year | 0 | firm + year, Grunfeld)
-se_lfe_2way   = se(est_lfe_2way)
+est_lfe_2way = felm(inv ~ capital | firm + year | 0 | firm + year, Grunfeld)
+se_lfe_2way = se(est_lfe_2way)
 se_feols_2way = se(est_feols, vcov = "twoway")
 rbind(se_lfe_2way, se_feols_2way)
 
@@ -172,6 +217,10 @@ rbind(pvalue(est_lfe_2way), pvalue(sum_feols_2way_conv))
 setFixest_ssc(ssc(K.adj = FALSE), "cluster")
 
 ## ---------------------------------------------------------------------------------------
-setFixest_vcov(no_FE = "iid", one_FE = "cluster", 
-               two_FE = "hetero", panel = "driscoll_kraay")
+setFixest_vcov(
+  no_FE = "iid",
+  one_FE = "cluster",
+  two_FE = "hetero",
+  panel = "driscoll_kraay"
+)
 
